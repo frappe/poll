4011076a7b8dd306169c7c8a5561bafe76377eaa
Rushabh Mehta <rmehta@gmail.com>
2014-07-03 14:45:58 +0530

    removed print

--- poll/templates/pages/polls.py
+++ poll/templates/pages/polls.py
@@ -8,8 +8,6 @@ def get_context(context):
 	context.listed_polls = frappe.get_list("Poll",
 		fields = ["title", "page_name", "description", "poll_status", "parent_website_route"],
 		filters={"published":1}, ignore_permissions=True)
-	#context.parents =
 	context.std_footer = frappe.db.get_value("Poll Settings", fieldname = "standard_footer")
 	context.std_header = frappe.db.get_value("Poll Settings", fieldname = "standard_header")
-	print context.listed_polls
 	return context
